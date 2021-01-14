11535e37bf7368b20e35473a73cb2d72bce063f1
Robert_Norwood <robnorwood91@gmail.com>
2021-01-13 16:53:59 -0800

    Created the contact item component and ccontact component.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,61 @@ import React, { useReducer } from 'react';
 import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@gmail.com',
+        phone: '111-111-1111',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Forrest Gump',
+        email: 'forrest@gmail.com',
+        phone: '111-111-2222',
+        type: 'professional',
+      },
+      {
+        id: 3,
+        name: 'Olivia Munn',
+        email: 'olivia@gmail.com',
+        phone: '111-111-3333',
+        type: 'wife',
+      },
+    ],
+  };
+
+  //dispatch allows us to dispatch object to the reducer
+  //state allows us to access anything in our state
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+  //Add contacts
+
+  //Delete Contact
+  //Set Current Contact
+  //Clear Current Contact
+  //Update Contact
+  //Filter contacts
+  //Clear Filter
+
+  return (
+    //Anything you want any component to access throughout the app goes inside the value brackets
+    <ContactContext.Provider value={{ contacts: state.contacts }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
